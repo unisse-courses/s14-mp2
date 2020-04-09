c6956431df6654ddd27871cce1848b47822cc530
Jazzmine07 <61366865+Jazzmine07@users.noreply.github.com>
2020-04-09 23:12:18 +0800

    Phase 2
    Phase 2

--- models/donation.js
+++ models/donation.js
@@ -1,11 +1,14 @@
 const mongoose = require('./connection');
 
 const donateSchema = new mongoose.Schema({
-  postID: { type: Number, required: true},
-  donationID: { type: Number, required: true,},
-  donationAmount: { type:Number, required: true},
-  accountID: { type: Number, required: false},
   donor:{ type: String, required: false},
+  cardNumber: { type: Number, required: false },
+  cvv: { type: Number, required: false },
+  postID: { type: Number, required: true},
+  expDate: { type: Date, required: false },
+  donationAmount: { type:Number, required: true},
+  donationID: { type: Number, required: true,},
+  accountID: { type: Number, required: false},
 });
 
 const Donation = mongoose.model('donate', donateSchema);
